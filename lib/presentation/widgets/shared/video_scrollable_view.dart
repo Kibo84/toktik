2860e751c5c183e5047d62f722823e9a75ccd0e4
Kibo84 <100001143+Kibo84@users.noreply.github.com>
2023-10-14 12:02:07 +0200

    feat (video player) add and configure video_player dependency

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_player/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/full_screen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -16,6 +17,10 @@ class VideoScrollableView extends StatelessWidget {
         final VideoPost videoPost = videos[index];
         return Stack(
           children: [
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                  caption: videoPost.caption, videoUrl: videoPost.videoUrl),
+            ),
             Positioned(
                 bottom: 40, right: 20, child: VideoButtons(video: videoPost)),
           ],
